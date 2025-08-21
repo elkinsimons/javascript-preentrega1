634106e2f4b3c627b45fa01c1cf2c8a24d16c78e
Elkin Simons <elkin.simons@gmail.com>
2025-08-21 05:10:54 -0300

    Actualización.

--- js/myScript.js
+++ js/myScript.js
@@ -10,48 +10,59 @@ alert("Hola, para poder darle un turno voy a solicitar que complete unos datos. 
 function tomarDatosPersonales() {
   let nombre = prompt("Ingrese su primer nombre:");
   let apellido = prompt("Ingrese su apellido:");
-  let edad = prompt("Ingrese su edad:");
-  if (edad <= 17) { 
-    alert("Si eres menor de edad va a ser necesario que te acompañe un mayor de edad"); 
+  console.log("Nombre:",nombre );
+  console.log("Apellido:",apellido );
+
+  function edades() {
+    let edad = prompt("Ingrese su edad:");  
+
+    if (edad <= 17) { 
+    alert("Si eres menor de edad va a ser necesario que te acompañe un adulto."); 
     let nombreTutor = prompt("Ingrese nombre y apellido de su tutor o acompañante:");
     let edadTutor = prompt("Ingrese la edad de su tutor o acompañante:");
     console.log("Edad:",edad );
     console.log("Nombre del tutor:",nombreTutor );
     console.log("Edad del tutor:",edadTutor );
-    } else if (edad => 65){
+    
+    function aranceles(){
+      let duracionSesion = prompt("¿Prefieres una sesión de 60 o 90 minutos?");
+      let precio1 = "$25000";
+      let precio2 = "$40000";
+      
+      if (duracionSesion == 60){
+        console.log("Tu sesión de",duracionSesion,"minutos tiene un valor de", precio1, ".")
+        } else if (duracionSesion == 90){
+          console.log("Tu sesión de",duracionSesion,"minutos tiene un valor de", precio2, ".")
+          }
+        }
+
+        aranceles()
+
+      } else if (edad >= 65){
+      console.log("Edad:",edad );
       let confirmarJubilacion = confirm("¿Está usted jubilado/a?");
       console.log("Jubilado/a", confirmarJubilacion)
+
+      if (confirmarJubilacion == true) {
+        alert("Tiene un 20% de descuento por ser jubilado/a.")
+          
+        let duracionSesion = prompt("¿Prefieres una sesión de 60 o 90 minutos?");
+        let precio1 = "25000";
+        let precio2 = "40000";
+        if (duracionSesion == 60){
+          console.log("Tu sesión de",duracionSesion,"minutos tiene un valor de $",precio1 * 0.8, ".")
+          } else if (duracionSesion == 90){
+            console.log("Tu sesión de",duracionSesion,"minutos tiene un valor de $",precio2 * 0.8, ".")
+        }
       }
-    
-  //Validar edades con alerta y sugerencia de acompañamiento con otra función.
-     // si edad menor/igual a 17, sugerir acompañamiento
-     // si edad mayor a 65 consultar por jubilacion
-  
-  // Consultar duración de la sesión: 60 o 90 e informar sus valores en el alert   
-     // Aplicar descuento del 20% si la persona es jubilada
-  
+    }
+  }
 
+  edades ()
+  
   let zonaDeDolor = prompt("¿Qué partes de tu cuerpo te duelen?  (Ej.: Espalda, brazos, cintura, etc.)");
-  console.log("Nombre:",nombre );
-  console.log("Apellido:",apellido );
   
-  //  console.log("Tu sesión tendrá un valor de:", descuentoJubilacion );
-  
-  
-    // } else if (confirmarJubilacion == true){let descuentoJubilacion = 60 * 0.8;}
-
-  console.log("Siento dolor en",zonaDeDolor );
+  console.log("Dolores en:",zonaDeDolor );
 }
 
-// Presupuesto del servicio
-
 tomarDatosPersonales()
-
-alert("¡Gracias por completar sus datos!" );
-
-// alert("Se aplicó un descuento del 20% por jubilación.");
-
-alert("Tu (sesión de una hora/hora y media) tendrá un valor de ($$)" );
-// Condiciones para generar el descuento.
-
-// Define mensajes claros a mostrar en los cuadros de diálogo. Concatena textos con variables, y realiza saltos de línea en textos extensos. Esto facilitará su lectura 
